aeac84faecf092969a77b5031573e16d3a1f3b3e
cwissett <cwissett@snapchat.com>
2026-07-02 22:12:43 +0100

    fix: 4-edge chaser highlight (renders on WebKit) (v19)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'serial-scanner-v18';
+const CACHE = 'serial-scanner-v19';
 const ASSETS = [
   './', './index.html', './css/style.css',
   './js/app.js', './js/serial.js', './js/vote.js', './js/store.js',
